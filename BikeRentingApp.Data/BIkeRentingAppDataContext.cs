3a742d3496e91395c868d4c5bf9cc398a1c2fdbd
vinushachandran <80586774+vinushachandran@users.noreply.github.com>
2025-05-17 19:47:51 +0530

    Add project files.

---
+++ BikeRentingApp.Data/BIkeRentingAppDataContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeRentingApp.Data
+{
+    public class BIkeRentingAppDataContext:DbContext
+    {
+        public BIkeRentingAppDataContext(DbContextOptions<BIkeRentingAppDataContext> options) : base(options) { }
+    }
+}
